5fbd30d7725fd768f9d88d9125d108c800acf0b5
Tolga Karahan <karahantlg@gmail.com>
2022-05-06 15:08:17 +0300

    added couler examples to implement workflows and DAGs in Python

---
+++ intro/couler_workflow.py
@@ -0,0 +1,33 @@
+# Couler library can be used to write workflows
+# that will be run via Argo. Couler provides an
+# unified interface to implement workflows to 
+# run via different orchestrators such as: Argo,
+# Airflow.
+import couler.argo as couler
+from couler.argo_submitter import ArgoSubmitter
+
+
+def random_code():
+    import random
+    res = "heads" if random.randint(0, 1) == 0 else "tails"
+    print(res)
+
+def flip_coin():
+    return couler.run_script(image="python:alpine3.6", source=random_code)
+
+def heads():
+    return couler.run_container(
+        image="alpine:3.6", command=["sh", "-c", 'echo "it was heads"']
+    )
+
+def tails():
+    return couler.run_container(
+        image="alpine:3.6", command=["sh", "-c", 'echo "it was tails"']
+    )
+
+result = flip_coin()
+couler.when(couler.equal(result, "heads"), lambda: heads())
+couler.when(couler.equal(result, "tails"), lambda: tails())
+
+submitter = ArgoSubmitter(namespace="argo")
+couler.run(submitter=submitter)
